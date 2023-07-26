245be15a9756f1583af64b5548f5e14e24bc8108
lanjudev16 <lanjudev16@gmail.com>
2023-07-26 22:11:55 +0600

    first commit

--- ReversedString.js
+++ ReversedString.js
@@ -5,4 +5,16 @@ const reversedString=(inputString)=>{
         }
         return reversedString.join('');
 }
-console.log(reversedString("Lanju"))
+// console.log(reversedString("hello world"))
+const sumOfAllPositiveNumber=(inputNumber)=>{
+    let sum=0;
+    for (const number of inputNumber) {
+        
+        if(number>0){
+            sum+=number
+        }
+    }
+    return sum
+}
+// console.log(sumOfAllPositiveNumber([2, -5, 10, -3, 7]))
+
