457b04f7bb095e0b84c90116d5444be717a0a1ee
lanjudev16 <lanjudev16@gmail.com>
2023-07-26 22:49:55 +0600

    last commit

--- SCPCTASK.js
+++ SCPCTASK.js
@@ -142,29 +142,6 @@ function romanToInteger(romanNumeral) {
   const romanNumeral2 = "XXI";
 //   console.log(`${romanNumeral1} -> ${romanToInteger(romanNumeral1)}`);
 //   console.log(`${romanNumeral2} -> ${romanToInteger(romanNumeral2)}`);
-function findSecondSmallest(arr) {
-    if (arr.length < 2) {
-      return "Array must have at least two elements";
-    }
-  
-    let smallest = Infinity;
-    let secondSmallest = Infinity;
-  
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < smallest) {
-        secondSmallest = smallest;
-        smallest = arr[i];
-      } else if (arr[i] < secondSmallest && arr[i] !== smallest) {
-        secondSmallest = arr[i];
-      }
-    }
-  
-    return secondSmallest;
-  }
-  
-  const array = [5, 8, 3, 1, 10, 7, 2];
-  const secondSmallest = findSecondSmallest(array);
-  console.log("Second Smallest:", secondSmallest); 
   
 
   
